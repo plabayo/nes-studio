0b2674c668d2473dcf99c9258ca69ea5d99a79d6
glendc <glen.decauwsemaecker@otainsight.com>
2023-10-19 00:13:18 +0200

    add initial nes-studio (egui-driven) project

--- justfile
+++ justfile
@@ -7,5 +7,11 @@ watch:
 deploy:
     cargo shuttle deploy
 
-wasm-pack:
-    wasm-pack build --target web --release ./nes-studio
+trunk:
+    cargo check --workspace --all-targets
+    cargo check --workspace --all-features --lib --target wasm32-unknown-unknown
+    cargo fmt --all -- --check
+    cargo clippy --workspace --all-targets --all-features --  -D warnings -W clippy::all
+    cargo test --workspace --all-targets --all-features
+    cargo test --workspace --doc
+    cd nes-studio && trunk build
--- nes-studio-website/src/main.rs
+++ nes-studio-website/src/main.rs
@@ -1,12 +1,22 @@
-use axum::{routing::get, Router};
+use std::path::PathBuf;
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
-}
+use axum::Router;
+use tower::ServiceBuilder;
+use tower_http::{
+    compression::CompressionLayer, normalize_path::NormalizePathLayer, services::ServeDir,
+    trace::TraceLayer,
+};
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .nest_service("/", ServeDir::new(PathBuf::from("nes-studio/dist")))
+        .layer(
+            ServiceBuilder::new()
+                .layer(TraceLayer::new_for_http())
+                .layer(CompressionLayer::new())
+                .layer(NormalizePathLayer::trim_trailing_slash()),
+        );
 
     Ok(router.into())
 }
